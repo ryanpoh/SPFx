9e61afbc26867228895d2b7af0ca6d2cb5e3edca
Ryan Poh <ryan.poh@gmail.com>
2019-12-12 12:46:26 +0800

    addding more properties to web part helloworld
    2

--- helloworld-webpart/src/webparts/helloWorld/HelloWorldWebPart.ts
+++ helloworld-webpart/src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -1,31 +1,46 @@
-import { Version } from '@microsoft/sp-core-library';
+import { Version } from "@microsoft/sp-core-library";
 import {
   BaseClientSideWebPart,
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-webpart-base';
-import { escape } from '@microsoft/sp-lodash-subset';
+  PropertyPaneTextField,
+  PropertyPaneCheckbox,
+  PropertyPaneDropdown,
+  PropertyPaneToggle
+} from "@microsoft/sp-webpart-base";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-import styles from './HelloWorldWebPart.module.scss';
-import * as strings from 'HelloWorldWebPartStrings';
+import styles from "./HelloWorldWebPart.module.scss";
+import * as strings from "HelloWorldWebPartStrings";
 
 export interface IHelloWorldWebPartProps {
   description: string;
+  test: string;
+  test1: boolean;
+  test2: string;
+  test3: boolean;
 }
 
-export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
-
+export default class HelloWorldWebPart extends BaseClientSideWebPart<
+  IHelloWorldWebPartProps
+> {
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${ styles.helloWorld }">
-        <div class="${ styles.container }">
-          <div class="${ styles.row }">
-            <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint!</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
+      <div class="${styles.helloWorld}">
+        <div class="${styles.container}">
+          <div class="${styles.row}">
+            <div class="${styles.column}">
+              <span class="${styles.title}">Welcome to SharePoint!</span>
+              <p class="${
+                styles.subTitle
+              }">Customize SharePoint experiences using Web Parts.</p>
+              <p class="${styles.description}">${escape(
+      this.properties.description
+    )}</p>
+    <p class="${styles.description}">${escape(
+      this.properties.test
+    )}</p>
+              <a href="https://aka.ms/spfx" class="${styles.button}">
+                <span class="${styles.label}">Learn more</span>
               </a>
             </div>
           </div>
@@ -34,7 +49,7 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -48,8 +63,29 @@ export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorld
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
+                PropertyPaneTextField("description", {
+                  label: "Description"
+                }),
+                PropertyPaneTextField("test", {
+                  label: "Multi-line Text Field",
+                  multiline: true
+                }),
+                PropertyPaneCheckbox("test1", {
+                  text: "Checkbox"
+                }),
+                PropertyPaneDropdown("test2", {
+                  label: "Dropdown",
+                  options: [
+                    { key: "1", text: "One" },
+                    { key: "2", text: "Two" },
+                    { key: "3", text: "Three" },
+                    { key: "4", text: "Four" }
+                  ]
+                }),
+                PropertyPaneToggle("test3", {
+                  label: "Toggle",
+                  onText: "On",
+                  offText: "Off"
                 })
               ]
             }
