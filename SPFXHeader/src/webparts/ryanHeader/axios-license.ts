c541fe37ad495bf9596c758c193dae29530cbed1
Ryan Poh <ryan.poh@gmail.com>
2020-02-19 11:55:32 +0800

    minor

--- SPFXHeader/src/webparts/ryanHeader/axios-license.ts
+++ SPFXHeader/src/webparts/ryanHeader/axios-license.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-//! When setting up Firebase, there is some sort of 30 Day database disable feature. If got problem, it should be because of that.
 const instance = axios.create({
   baseURL:
     "https://firestore.googleapis.com/v1/projects/srkklicense/databases/(default)/documents/"
